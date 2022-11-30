9d01076b2470a97f39b3a7ddc0cf3d0f9da2073d
ombasaabuya <ombasabuya@gmail.com>
2022-11-30 12:55:48 +0300

    Completed assignment

--- grade.js
+++ grade.js
@@ -1,16 +1,15 @@
-let studentMarks = parseInt(prompt('Kindly Enter Student Marks:'));
-function checkStudentGrade(studentMarks)
-{
-    if (studentMarks > 79 && studentMarks < 100) {
+
+function checkStudentGrade(studentMarks){
+    if (studentMarks > 79 && studentMarks <= 100) {
         console.log("Grade: A");
         
-      } else if (studentMarks >= 60 && studentMarks <= 79) {
+      } else if (studentMarks > 60 && studentMarks <= 79) {
         console.log("Grade : B");
 
       } else if (studentMarks > 49 && studentMarks <= 59) {
         console.log("Grade : C");
  
-      } else if (studentMarks > 40 && studentMarks <= 49) {
+      } else if (studentMarks >= 40 && studentMarks <= 49) {
         console.log("Grade : D");
 
       } else if (studentMarks >= 0 && studentMarks <= 40) {
@@ -20,6 +19,5 @@ function checkStudentGrade(studentMarks)
         console.log("Enter Valid marks")        
       }
 }
-document.getElementById("studentMarks").innerHTML = studentMarks;
 
 //console.log(checkStudentGrade(studentMarks));
